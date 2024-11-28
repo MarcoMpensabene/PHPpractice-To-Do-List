d9e8dfe0c30fb2c1b41e7fb3bc30200dd4c27935
MarcoMpensabene <marcompensabene@gmail.com>
2024-11-28 13:26:54 +0100

    created db connection with database.php and mysql

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'todo_app';
+$username = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Errore di connessione: " . $e->getMessage());
+}
+
+return $pdo;
